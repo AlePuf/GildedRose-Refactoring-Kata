50db27318cff9611572ac5a34f07c37e5753cb19
Alex Puflene <Alex.Puflene@softwire.com>
2022-06-29 16:32:54 +0300

    Fixed suggestions 2

--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -3,144 +3,144 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
-    //given
+    // given
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    //when
+    // when
     const items = gildedRose.updateQuality();
-    //then
+    // then
     expect(items[0].name).to.equal('foo');
   });
 
   it('should degrade', () => {
-    //given
+    // given
     const gildedRose = new GildedRose([new Item('Cheese', 5, 20)]);
-    //when
+    // when
     const items = gildedRose.updateQuality();
-    //then
+    // then
     expect(items[0].quality).to.equal(19);
   });
 
   it('should degrade twice as fast past the expiration date', () => {
-    //given
+    // given
     const gildedRose = new GildedRose([new Item('Cheese', 0, 20)]);
-    //when
+    // when
     const items = gildedRose.updateQuality();
-    //then
+    // then
     expect(items[0].quality).to.equal(18);
   });
 
   it('should not have negative quality', () => {
-    //given
+    // given
     const gildedRose = new GildedRose([new Item('test', 0, 0)]);
-    //when
+    // when
     const items = gildedRose.updateQuality();
-    //then
+    // then
     expect(items[0].quality).to.equal(0);
   });
 
   it('should not increase quality past 50', () => {
-    //given
+    // given
     const gildedRose = new GildedRose([new Item('Aged Brie', 0, 50)]);
-    //when
+    // when
     const items = gildedRose.updateQuality();
-    //then
+    // then
     expect(items[0].quality).to.equal(50);
   });
 
   describe('Aged Brie', () => {
-    it('should increase quality for aged brie', () => {
-      //given
+    it('should increase quality', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Aged Brie', 2, 25)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(26);
     });
 
-    it('should increase quality by 2 for aged brie past sellIn', () => {
-      //given
+    it('should increase quality by 2 past sellIn', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Aged Brie', 0, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(32);
     });
   });
 
   describe('Sulfuras', () => {
-    it('should not decrease quality for Sulfuras', () => {
-      //given
+    it('should not decrease quality', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 30, 50)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(50);
     });
 
-    it('should not decrease sellIn for Sulfuras', () => {
-      //given
+    it('should not decrease sellIn', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 30, 50)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].sellIn).to.equal(30);
     });
   });
 
   describe('Backstage passes', () => {
-    it('should increase quality for backstage passes', () => {
-      //given
+    it('should increase quality', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(31);
     });
 
-    it('should increase quality for backstage passes by 2 when 10 days are left', () => {
-      //given
+    it('should increase quality by 2 when 10 days are left', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(32);
     });
 
-    it('should increase quality for backstage passes by 3 when 5 days are left', () => {
-      //given
+    it('should increase quality by 3 when 5 days are left', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 40)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(43);
     });
 
-    it('should set quality for backstage passes to 0 after the date', () => {
-      //given
+    it('should set quality to 0 after the date', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(0);
     });
   });
 
   describe('Conjured', () => {
-    it('should decrease Conjured quality by 2', () => {
-      //given
+    it('should decrease quality by 2', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Conjured', 10, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(28);
     });
 
-    it('should decrease Conjured quality by 4 past sellin', () => {
-      //given
+    it('should decrease quality by 4 past sellIn', () => {
+      // given
       const gildedRose = new GildedRose([new Item('Conjured', 0, 30)]);
-      //when
+      // when
       const items = gildedRose.updateQuality();
-      //then
+      // then
       expect(items[0].quality).to.equal(26);
     });
   });
